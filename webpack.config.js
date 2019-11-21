de070ec6e0573370eb23a651e8d21b08a09fe491
Anthony Hien Vu <anthony.vu@team.telstra.com>
2019-11-21 15:44:57 +0800

    Changed webpack in an attempt to resolve heroku production commit bug.

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,27 @@
-// Decribe how our app should be bundled.
-const path = require("path");
+// import path from 'path';
 
-// Configuration that webpack checks when we run it.
+const path = require("path");
+// export default {
 module.exports = {
-  mode: "development", // can be "development" or "production"
-  // https://nodejs.org/api/path.html#path_path_resolve_paths
-  entry: path.resolve(__dirname, `src`, `app`), // __dirname is a special property which is always the directory of the file which has been run from.
-  // Definition of the one file that our application's going to become.
+  mode: "development",
+  entry: path.resolve(__dirname, "src", "app"),
   output: {
-    path: path.resolve(__dirname, 'dist'), // 'dist' for built finished files.
-    filename: 'bundle.js',
-    publicPath: '/' // Where our code expects to find this file.
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/"
   },
-  // Array of the extensions that we want webpack to process.
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
-  // Setting we have to enable if we want to use React Router.
   devServer: {
     historyApiFallback: true
   },
-  // Regex that application will use to determine if a file should or should not be compiled.
   module: {
-    rules: [{
-      test: /\.jsx?/, // Any file with .js or maybe x at the end, which means js or jsx matches the test.
-      loader: 'babel-loader' // Defines a loader that we want to use for that file.
-    }]
+    rules: [
+      {
+        test: /\.jsx?/,
+        loader: "babel-loader"
+      }
+    ]
   }
-}
+};
